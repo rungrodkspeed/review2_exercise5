02a5b5116a83e5a2f04a8794cf9c2658d3b05759
unknown <rungrodks@hotmail.com>
2019-01-25 01:52:41 +0700

    .

---
+++ ข้อ3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int A[6]={9,-7,3,4,-2,4};
+    int i=0;
+    while(i<6){
+        cout << A[i] << endl;
+        i++;
+    }
+    return 0;
+}
