657157eaa11954d83dbfbd68744d25d5eb85dc31
unknown <rungrodks@hotmail.com>
2019-01-25 01:55:52 +0700

    .

---
+++ ข้อ4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int A[6]={9,-7,3,4,-2,4};
+    int i=0;
+    while(i<6){
+        if(A[i]>0){
+            cout << A[i]<<endl;
+        }
+        i++;
+    }
+    return 0;
+}
